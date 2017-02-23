9fd9673f59fc36dd38999abb3ecaed44454eb47f
Bryce Allen <bryce@freshwatermap.com>
2017-02-23 14:42:38 -0600

    more info in out of bounds printf

--- sztest.c
+++ sztest.c
@@ -4,8 +4,12 @@
 
 #include "sz.h"
 
+#define SET_DATA_ROWS 2
+#define SET_DATA_COLS 4
 
-double check_err(double *a, double *b, size_t n, double abs_bound);
+
+double check_err(double *a, double *b, size_t n,
+                 double *out_max_diff, int *out_max_diff_idx);
 void *malloc_or_die(size_t n);
 unsigned char *test_roundtrip(double *data, unsigned int n,
                               double abs_bound,
@@ -32,12 +36,12 @@ int main(int argc, char **argv) {
     double *rand_data = malloc_or_die(data_size);
     double *lin_data = malloc_or_die(data_size);
     double *const_data = malloc_or_die(data_size);
+    double *near1_data = malloc_or_die(data_size);
 
-    double set_data[2][3] = {
-        { 1.0, 383.2384, -77777777777 },
-        { 68123.00, 0.007, 99.99999 },
+    double set_data[SET_DATA_ROWS][SET_DATA_COLS] = {
+        { 1.0, 383.2384, -77777777777, 111111112128178324 },
+        { 68123.00, 0.007, 99.99999, 111111112128178329 },
     };
-    double set_data2[2][3];
 
     srand48(2895720909174927L);
 
@@ -51,6 +55,7 @@ int main(int argc, char **argv) {
             rand_data[ij] = drand48() * 1024;
             lin_data[ij] = v;
             const_data[ij] = 1.0;
+            near1_data[ij] = 1.0 + drand48();
             v += 0.1;
             //printf("%d %d %p\n", i, j, &rand_data[i][j]);
         }
@@ -74,33 +79,43 @@ int main(int argc, char **argv) {
     unsigned char *const_out =
         test_roundtrip(const_data, n, abs_bound, 0, 0, 0, rows, cols);
 
+    printf("\n== NEAR1 ==\n");
+    unsigned char *near1_out =
+        test_roundtrip(near1_data, n, abs_bound, 0, 0, 0, rows, cols);
+
     printf("\n== SET ==\n");
     unsigned char *set_out =
-        test_roundtrip(&set_data[0][0], 2*3, abs_bound, 0, 0, 0, 2, 3);
+        test_roundtrip(&set_data[0][0], SET_DATA_ROWS*SET_DATA_COLS,
+                       abs_bound, 0, 0, 0, SET_DATA_ROWS, SET_DATA_COLS);
 
 
     free(rand_data);
     free(lin_data);
     free(const_data);
+    free(near1_data);
 
     free(set_out);
     free(rand_out);
     free(lin_out);
     free(const_out);
+    free(near1_out);
 
     SZ_Finalize();
 }
 
 
-// returns negative if any individual error is greater than abs_bound, otherwise
-// returns the average error.
-double check_err(double *a, double *b, size_t n, double abs_bound) {
+// Returns average error, and max diff and max diff index in out params.
+double check_err(double *a, double *b, size_t n,
+                 double *out_max_diff, int *out_max_diff_idx) {
     double diff;
     double total_error = 0;
+    *out_max_diff = 0;
+    *out_max_diff_idx = 0;
     for (int i=0; i<n; i++) {
         diff = fabs(a[i] - b[i]);
-        if (diff > abs_bound) {
-            return -1 * diff;
+        if (diff > *out_max_diff) {
+            *out_max_diff = diff;
+            *out_max_diff_idx = i;
         }
         total_error += diff;
     }
@@ -128,12 +143,18 @@ unsigned char *test_roundtrip(double *data, unsigned int n,
         printf("roundtrip differs\n");
     }
 
-    double avg_error = check_err(data, data2, n, abs_bound);
-    if (avg_error < 0) {
-        fprintf(stderr, "ERR: data error out of range: %0.16f\n", avg_error);
-    } else {
-        printf("avg error = %0.16f\n", avg_error);
+    double max_diff = 0;
+    int max_diff_idx = 0;
+
+    double avg_error = check_err(data, data2, n, &max_diff, &max_diff_idx);
+    if (max_diff > abs_bound) {
+        fprintf(stderr,
+                "ERR: data error out of range at [%d]: "
+                "orig %0.16f decomp %0.16f (diff %0.16f)\n",
+                max_diff_idx, data[max_diff_idx],
+                data2[max_diff_idx], max_diff);
     }
+    printf("avg error = %0.16f\n", avg_error);
 
     free(data2);
 
